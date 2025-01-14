5bae5f87f916ac382b2076e6296f2f86caf4257e
Lenarrateur <gaetansebastien1@gmail.com>
2025-01-14 14:19:24 +0100

    Added a Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='{ToEachIsPath-Gaetan_Peter-CDOF5}',
+version='1.0',
+author='{Lenarrateur}',
+license='MIT',
+long_description=open('README.md').read(),
+)
